4056220221e7d7bd84c95be5a49c853c6ff7f6a7
eugene <olympian2008@yandex.ru>
2022-09-28 21:35:35 +0300

    new files

--- Praktikum.java
+++ Praktikum.java
@@ -1,5 +1,8 @@
 class Praktikum {
-    public static void main(String args[]) { 
+    Praktikum() {
+    }	
 
+    public static void main(String args[]) { 
+	
     }	
 }
---
+++ newClass.java
@@ -0,0 +1,5 @@
+class newClass {
+    public static void main(String args[]) { 
+	
+    }
+}
